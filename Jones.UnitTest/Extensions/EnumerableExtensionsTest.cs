c9f063e64032a0892685c78ed1ba9a9dfe868fa9
innwin <innwin@live.com>
2019-08-20 15:18:42 +0800

    优化`Jones`下`EnumerableExtensions`中的`ToString`方法

---
+++ Jones.UnitTest/Extensions/EnumerableExtensionsTest.cs
@@ -0,0 +1,20 @@
+using System;
+using Jones.Extensions;
+using NUnit.Framework;
+
+namespace Jones.UnitTest.Extensions
+{
+    [TestFixture]
+    public class EnumerableExtensionsTest
+    {
+        [Test]
+        public void TestToString()
+        {
+            var intString = new [] { 1, 2}.ToString(",");
+            Console.WriteLine(intString);
+            Assert.AreEqual(intString, "1,2");
+            var stringString = new [] { "1", "2"}.ToString(",");
+            Assert.AreEqual(stringString, "1,2");
+        }
+    }
+}
--- Jones/Extensions/EnumerableExtensions.cs
+++ Jones/Extensions/EnumerableExtensions.cs
@@ -4,29 +4,14 @@ using System.Text;
 
 namespace Jones.Extensions
 {
-    public static class CollectionExtensions
+    public static class EnumerableExtensions
     {
         public static bool IsNullOrEmpty<T>(this IEnumerable<T> source)
         {
             return source == null || !source.Any();
         }
-        
-        public static string ToString<T>(this IEnumerable<T> source, string separated)
-        {
-            var enumerable = source as T[] ?? source.ToArray();
-            if (enumerable.IsNullOrEmpty())
-            {
-                return null;
-            }
-            var resultSbr = new StringBuilder();
-            foreach (var data in enumerable)
-            {
-                if (resultSbr.Length == 0)
-                    resultSbr.Append(data);
-                else
-                    resultSbr.Append(separated).Append(data);
-            }
-            return resultSbr.ToString();
-        }
+
+        public static string ToString<T>(this IEnumerable<T> source, string separated) =>
+            source.AsParallel().Select(p => p.ToString()).Aggregate((total, next) => $"{total}{separated}{next}");
     }
 }
