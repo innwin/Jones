75f3088e7b69e0af091fd1e46b41a59935b9b4e1
innwin <innwin@live.com>
2020-09-07 15:01:11 +0800

    `EnumExtensions`加入`CategoryAttribute`支持

--- Jones/Extensions/EnumExtensions.cs
+++ Jones/Extensions/EnumExtensions.cs
@@ -2,6 +2,7 @@ using System;
 using System.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Text;
 
 namespace Jones.Extensions
 {
@@ -22,6 +23,11 @@ namespace Jones.Extensions
         {
             return enumSubItem.GetAttribute<DescriptionAttribute>()?.Description;
         }
+
+        public static string? GetCategory(this Enum enumSubItem)
+        {
+            return enumSubItem.GetAttribute<CategoryAttribute>()?.Category;
+        }
         
         [return: MaybeNull]
         public static T GetEnumAttribute<T>(this Enum enumSubItem) where T : Attribute
@@ -36,6 +42,37 @@ namespace Jones.Extensions
         public static string? GetEnumDescription(this Enum enumSubItem)
         {
             return enumSubItem.GetEnumAttribute<DescriptionAttribute>()?.Description;
+        }
+
+        public static string? GetEnumCategory(this Enum enumSubItem)
+        {
+            return enumSubItem.GetEnumAttribute<CategoryAttribute>()?.Category;
+        }
+
+        public static string? GetEnumDescriptionAndCategory(this Enum enumSubItem)
+        {
+            var description = enumSubItem.GetEnumDescription();
+            var category = enumSubItem.GetEnumCategory();
+            if (description == null && category == null)
+            {
+                return null;
+            }
+            
+            var stringBuilder = new StringBuilder();
+            if (description != null)
+            {
+                stringBuilder.Append(description);
+            }
+
+            if (category != null)
+            {
+                if (stringBuilder.Length > 0)
+                {
+                    stringBuilder.Append(" : ");
+                }
+                stringBuilder.Append(category);
+            }
+            return stringBuilder.ToString();
         }
     }
 }
