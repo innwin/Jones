d59dfa0a23c228ac15923cc25f48f4eef30e2df9
innwin <innwin@live.com>
2020-09-12 09:08:00 +0800

    发布`Jones.AutoMapper`版本：v8.0.1

--- Jones.AutoMapper/AutoMapperExtensions.cs
+++ Jones.AutoMapper/AutoMapperExtensions.cs
@@ -11,6 +11,11 @@ namespace Jones.AutoMapper
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             return services;
         }
+        public static IServiceCollection AddJonesAutoMapper(this IServiceCollection services, Action<IMapperConfigurationExpression> configAction)
+        {
+            services.AddAutoMapper(configAction, AppDomain.CurrentDomain.GetAssemblies());
+            return services;
+        }
         
         public static T ToDto<T>(this IModel model, IMapper mapper) where T : IDto
         {
