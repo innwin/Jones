65b1a26d4d3771e8c6128b31427f86bbf054b8d4
innwin <innwin@live.com>
2021-08-03 16:43:46 +0800

    fixed `Jones`中的`TExtensions`.`IsNullable`

--- Jones/Extensions/TExtensions.cs
+++ Jones/Extensions/TExtensions.cs
@@ -4,13 +4,6 @@ namespace Jones.Extensions
 {
     public static class TExtensions
     {
-        public static bool IsNullable<T>(this T any)
-        {
-            if (any == null) return true; // obvious
-            var type = typeof(T);
-            if (!type.IsValueType) return true; // ref-type
-            if (Nullable.GetUnderlyingType(type) != null) return true; // Nullable<T>
-            return false; // value-type
-        }
+        public static bool IsNullable<T>(this T _) => Nullable.GetUnderlyingType(typeof(T)) != null;
     }
 }
