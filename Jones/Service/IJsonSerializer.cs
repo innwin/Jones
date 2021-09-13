2346d12843d037143f0e1ee843337ef449828b34
innwin <innwin@live.com>
2021-09-13 17:23:17 +0800

    `Jones`增加`IJsonSerializer`

---
+++ Jones/Service/IJsonSerializer.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Jones.Service
+{
+    public interface IJsonSerializer
+    {
+        TValue? Deserialize<TValue>(string json);
+        object? Deserialize(string json, Type returnType);
+        
+        string Serialize<TValue>(TValue value);
+        string Serialize(object? value, Type inputType);
+    }
+}
