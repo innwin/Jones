85d77f86631c8ee6453d22f66672312423401afc
innwin <innwin@live.com>
2021-11-03 11:59:38 +0800

    `Jones`中的`EnumerableExtensions`增加`Equal`方法

--- Jones/Extensions/EnumerableExtensions.cs
+++ Jones/Extensions/EnumerableExtensions.cs
@@ -13,6 +13,10 @@ namespace Jones.Extensions
         public static bool IsNotNullOrEmpty<T>(this IEnumerable<T>? source) => !source.IsNullOrEmpty();
         
         public static string ToString<T>(this IEnumerable<T> source, string separator) => string.Join(separator, source);
-            // source?.AsParallel().Select(p => p?.ToString() ?? string.Empty).Aggregate((total, next) => $"{total}{separated}{next}");
+        // source?.AsParallel().Select(p => p?.ToString() ?? string.Empty).Aggregate((total, next) => $"{total}{separated}{next}");
+        
+        public static bool Equal<T>(this IEnumerable<T>? first, IEnumerable<T>? second) =>
+            first == null && second == null
+            || first != null && second != null && first.SequenceEqual(second);
     }
 }
