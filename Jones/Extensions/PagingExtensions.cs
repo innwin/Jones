a3f678eab829885bb1870366005f1c97f6ece3e7
innwin <innwin@live.com>
2022-12-15 20:20:55 +0800

    重构 `DataReaderExtensions.To` 实现

---
+++ Jones/Extensions/PagingExtensions.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Linq;
+
+namespace Jones.Extensions;
+
+public static class PagingExtensions
+{
+    public static Paging<Tr> Transform<T, Tr>(this Paging<T> source, Func<T, Tr> transform) =>
+        new(source.Items.Select(transform).ToArray(), source.Page, source.PageSize, source.TotalCount);
+}
